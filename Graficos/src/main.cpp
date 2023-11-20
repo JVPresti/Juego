5f87ea4563b50eb4fd5f39370b521b634e60cb2d
JVPresti <nanojorgevazquez123@gmail.com>
2023-11-19 21:15:49 -0800

    Avance
    falta agregar signos, opcion2 y calculadora

--- Graficos/src/main.cpp
+++ Graficos/src/main.cpp
@@ -7,13 +7,13 @@
 #endif
 
 //----------------------------------------------------------------------------------
-// Some Defines
+// Algunas definiciones
 //----------------------------------------------------------------------------------
 #define SNAKE_LENGTH 256
 #define SQUARE_SIZE 30
 
 //----------------------------------------------------------------------------------
-// Types and Structures Definition
+// Tipos y definiciones de estructuras
 //----------------------------------------------------------------------------------
 typedef struct Snake
 {
@@ -28,11 +28,12 @@ typedef struct Food
     Vector2 position;
     Vector2 size;
     bool active;
+    bool operation;
     Color color;
 } Food;
 
 //------------------------------------------------------------------------------------
-// Global Variables Declaration
+// Declaraciones de variables globales
 //------------------------------------------------------------------------------------
 static const int screenWidth = 800;
 static const int screenHeight = 450;
@@ -47,31 +48,56 @@ static Vector2 snakePosition[SNAKE_LENGTH] = {0};
 static bool allowMove = false;
 static Vector2 offset = {0};
 static int counterTail = 0;
+static float menuAlpha = 1.0f;
+static bool menuFadeOut = false;
+int selectedOption = 0;
+bool fruitCollected = false;
 
-// Texture for background
-static Texture2D background;
+Music backgroundMusic;
+
+// Textura para el fondo del juego
+static Texture2D backgroundGame;
+
+// Textura para el fondo del menú
+static Texture2D backgroundMenu;
+
+// Estados del juego
+typedef enum
+{
+    MENU,
+    GAME
+} GameState;
+
+static GameState gameState = MENU;
 
 //------------------------------------------------------------------------------------
-// Module Functions Declaration (local)
+// Declaraciones de funciones locales
 //------------------------------------------------------------------------------------
-static void InitGame(void);        // Initialize game
-static void UpdateGame(void);      // Update game (one frame)
-static void DrawGame(void);        // Draw game (one frame)
-static void UnloadGame(void);      // Unload game
-static void UpdateDrawFrame(void); // Update and Draw (one frame)
-
+static void InitGame(void);        // Inicializa el juego
+static void UpdateGame(void);      // Actualiza el juego (un frame)
+static void DrawGame(void);        // Dibuja el juego (un frame)
+static void UnloadGame(void);      // Descarga recursos del juego
+static void UpdateDrawFrame(void); // Actualiza y dibuja (un frame)
+static void DrawMenu(void);        // Dibuja el menú
+static void UpdateMenu(void);      // Actualiza el menú
+void ResetGame(void);              // Reinicia el juego
+Font font;
 //------------------------------------------------------------------------------------
-// Program main entry point
+// Punto de entrada principal del programa
 //------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization (Note windowTitle is unused on Android)
+    // Inicialización (windowTitle no se usa en Android)
     //---------------------------------------------------------
-    InitWindow(screenWidth, screenHeight, "snake");
-    // Load background image
-    // Load background image with correct format
-    // Load background image with correct format
-    background = LoadTexture("C:\\Users\\nanoj\\OneDrive\\Escritorio\\snakefondo2.png");
+    InitWindow(screenWidth, screenHeight, "Serpent Snake");
+
+    // Cargar imágenes de fondo
+    backgroundGame = LoadTexture("C:\\Users\\nanoj\\OneDrive\\Escritorio\\snakefondo2.png");
+    backgroundMenu = LoadTexture("C:\\Users\\nanoj\\OneDrive\\Escritorio\\fondo.png");
+    font = LoadFont("C:\\Users\\nanoj\\OneDrive\\Escritorio\\Valoon.ttf");
+    // backgroundMusic = LoadMusicStream("C:\\Users\\nanoj\\OneDrive\\Escritorio\\musicafondo.mp3");
+
+    // PlayAudioStream(backgroundMusic);
 
     InitGame();
 
@@ -81,30 +107,26 @@ int main(void)
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose()) // Detect window close button or ESC key
+    // Bucle principal del juego
+    while (!WindowShouldClose()) // Detectar botón de cierre de la ventana o tecla ESC
     {
-        // Update and Draw
+        // Actualizar y Dibujar
         //----------------------------------------------------------------------------------
         UpdateDrawFrame();
         //----------------------------------------------------------------------------------
     }
 #endif
-    // De-Initialization
+    // Descarga de recursos
     //--------------------------------------------------------------------------------------
-    UnloadGame(); // Unload loaded data (textures, sounds, models...)
+    UnloadGame(); // Descargar datos cargados (texturas, sonidos, modelos...)
 
-    CloseWindow(); // Close window and OpenGL context
+    CloseWindow(); // Cierra la ventana y el contexto de OpenGL
     //--------------------------------------------------------------------------------------
 
     return 0;
 }
 
-//------------------------------------------------------------------------------------
-// Module Functions Definitions (local)
-//------------------------------------------------------------------------------------
-
-// Initialize game variables
+// Inicializa las variables del juego
 void InitGame(void)
 {
     framesCounter = 0;
@@ -139,9 +161,10 @@ void InitGame(void)
     fruit.active = false;
 }
 
-// Update game (one frame)
+// Actualiza el juego (un frame)
 void UpdateGame(void)
 {
+    int fruitCollected = 0;
     if (!gameOver)
     {
         if (IsKeyPressed('P'))
@@ -149,7 +172,7 @@ void UpdateGame(void)
 
         if (!pause)
         {
-            // Player control
+            // Control del jugador
             if (IsKeyPressed(KEY_RIGHT) && (snake[0].speed.x == 0) && allowMove)
             {
                 snake[0].speed = (Vector2){SQUARE_SIZE, 0};
@@ -171,7 +194,7 @@ void UpdateGame(void)
                 allowMove = false;
             }
 
-            // Snake movement
+            // Movimiento de la serpiente
             for (int i = 0; i < counterTail; i++)
                 snakePosition[i] = snake[i].position;
 
@@ -190,22 +213,23 @@ void UpdateGame(void)
                 }
             }
 
-            // Wall behaviour
+            // Comportamiento de la pared
             if (((snake[0].position.x) > (screenWidth - offset.x)) ||
                 ((snake[0].position.y) > (screenHeight - offset.y)) ||
                 (snake[0].position.x < 0) || (snake[0].position.y < 0))
             {
                 gameOver = true;
+                ResetGame();
             }
 
-            // Collision with yourself
+            // Colisión con uno mismo
             for (int i = 1; i < counterTail; i++)
             {
                 if ((snake[0].position.x == snake[i].position.x) && (snake[0].position.y == snake[i].position.y))
                     gameOver = true;
             }
 
-            // Fruit position calculation
+            // Cálculo de la posición de la fruta
             if (!fruit.active)
             {
                 fruit.active = true;
@@ -221,13 +245,13 @@ void UpdateGame(void)
                 }
             }
 
-            // Collision
-            if ((snake[0].position.x < (fruit.position.x + fruit.size.x) && (snake[0].position.x + snake[0].size.x) > fruit.position.x) &&
-                (snake[0].position.y < (fruit.position.y + fruit.size.y) && (snake[0].position.y + snake[0].size.y) > fruit.position.y))
+            // Colisión
+            if ((snake[0].position.x == fruit.position.x) && (snake[0].position.y == fruit.position.y))
             {
                 snake[counterTail].position = snakePosition[counterTail - 1];
                 counterTail += 1;
                 fruit.active = false;
+                fruitCollected = true;
             }
 
             framesCounter++;
@@ -235,28 +259,32 @@ void UpdateGame(void)
     }
     else
     {
-        if (IsKeyPressed(KEY_ENTER))
+        if (IsKeyPressed(KEY_ENTER) && !fruitCollected)
         {
             InitGame();
             gameOver = false;
+            gameState = GAME; // Cambia al estado del juego después de presionar ENTER
+        }
+        else
+        {
+            fruitCollected = false; // Reinicia la variable para la próxima vez
         }
     }
 }
 
-// Draw game (one frame)
+// Dibuja el juego (un frame)
 void DrawGame(void)
 {
-    // Declara numberText al principio de la función
     char numberText[5];
     Vector2 bkPosition = {0.0f, 0.0f};
     BeginDrawing();
 
-    // Draw background image
-    DrawTextureEx(background, bkPosition, 0, 0.62, RAYWHITE);
+    // Dibuja la imagen de fondo del juego
+    DrawTextureEx(backgroundGame, bkPosition, 0, 0.62, RAYWHITE);
 
     if (!gameOver)
     {
-        // Draw grid lines
+        // Dibuja las líneas de la cuadrícula
         for (int i = 0; i < screenWidth / SQUARE_SIZE + 1; i++)
         {
             DrawLineV((Vector2){SQUARE_SIZE * i + offset.x / 2, offset.y / 2}, (Vector2){SQUARE_SIZE * i + offset.x / 2, screenHeight - offset.y / 2}, DARKGREEN);
@@ -267,22 +295,16 @@ void DrawGame(void)
             DrawLineV((Vector2){offset.x / 2, SQUARE_SIZE * i + offset.y / 2}, (Vector2){screenWidth - offset.x / 2, SQUARE_SIZE * i + offset.y / 2}, DARKGREEN);
         }
 
-        // Draw snake
+        // Dibuja la serpiente
         for (int i = 0; i < counterTail; i++)
             DrawRectangleV(snake[i].position, snake[i].size, snake[i].color);
 
-        // Fruit position calculation
+        // Cálculo de la posición de la fruta
         if (!fruit.active)
         {
             fruit.active = true;
-
-            // Genera un número aleatorio entre 1 y 20
             int randomNumber = GetRandomValue(1, 20);
-
-            // Convierte el número a una cadena de caracteres
             sprintf(numberText, "%d", randomNumber);
-
-            // Asigna una nueva posición a la fruta
             fruit.position = (Vector2){GetRandomValue(0, (screenWidth / SQUARE_SIZE) - 1) * SQUARE_SIZE + offset.x / 2, GetRandomValue(0, (screenHeight / SQUARE_SIZE) - 1) * SQUARE_SIZE + offset.y / 2};
         }
 
@@ -293,24 +315,143 @@ void DrawGame(void)
         DrawText(numberText, fruit.position.x + fruit.size.x / 2 - MeasureText(numberText, 20) / 2, fruit.position.y + fruit.size.y / 2 - 10, 20, RED);
 
         if (pause)
-            DrawText("GAME PAUSED", screenWidth / 2 - MeasureText("GAME PAUSED", 40) / 2, screenHeight / 2 - 40, 40, GRAY);
+            DrawText("PAUSA", screenWidth / 2 - MeasureText("PAUSA", 40) / 2, screenHeight / 2 - 40, 40, GRAY);
     }
     else
-        DrawText("PRESS [ENTER] TO PLAY AGAIN", GetScreenWidth() / 2 - MeasureText("PRESS [ENTER] TO PLAY AGAIN", 20) / 2, GetScreenHeight() / 2 - 50, 20, GRAY);
+        DrawText("PRESIONA [ENTER] PARA JUGAR DE NUEVO", GetScreenWidth() / 2 - MeasureText("PRESIONA [ENTER] PARA JUGAR DE NUEVO", 20) / 2, GetScreenHeight() / 2 - 50, 20, GRAY);
 
     EndDrawing();
 }
 
-// Unload game variables
+// Descarga recursos del juego
 void UnloadGame(void)
 {
-    // Unload background image
-    UnloadTexture(background);
+    // Descarga las texturas de fondo
+    UnloadTexture(backgroundGame);
+    UnloadTexture(backgroundMenu);
+    UnloadMusicStream(backgroundMusic);
+    UnloadFont(font);
 }
 
-// Update and Draw (one frame)
+// Actualiza y dibuja (un frame)
 void UpdateDrawFrame(void)
 {
-    UpdateGame();
-    DrawGame();
+    if (gameState == MENU)
+    {
+        UpdateMenu();
+        DrawMenu();
+        if (menuAlpha == 0.0f)
+        {
+            if (selectedOption == 1)
+            {
+                // Inicia el juego después de que el menú se desvanece
+                InitGame();
+                gameOver = false;
+                gameState = GAME;
+            }
+            else if (selectedOption == 2)
+            {
+                // Lógica para la opción 2 después de que el menú se desvanece
+                // ... Agrega la lógica adicional aquí ...
+            }
+            else if (selectedOption == 3)
+            {
+                // Cierra la ventana después de que el menú se desvanece
+                CloseWindow();
+            }
+        }
+    }
+    else if (gameState == GAME)
+    {
+        UpdateGame();
+        DrawGame();
+
+        // Verifica si el juego ha terminado y se ha presionado Enter
+        if (gameOver && IsKeyPressed(KEY_ENTER))
+        {
+            InitGame();
+            gameOver = false;
+            gameState = GAME;
+            fruitCollected = false; // Asegúrate de reiniciar la variable para la próxima vez
+            ResetGame();
+        }
+    }
+}
+
+// Dibuja el menú
+void DrawMenu(void)
+{
+    Vector2 bkPosition = {0.0f, 0.0f};
+    BeginDrawing();
+
+    // Dibuja la imagen de fondo del menú con opacidad
+    DrawTextureEx(backgroundMenu, bkPosition, 0, 0.85, Fade(RAYWHITE, menuAlpha));
+
+    // Realiza conversiones explícitas de int a float para evitar warnings
+    DrawTextEx(font, "Operation Snake", (Vector2){(float)(screenWidth / 2 - MeasureText("Operation Snake", 40) / 2), (float)(screenHeight / 2 - 40)}, 40, 0, BLACK);
+    DrawTextEx(font, "1. Nivel Facil", (Vector2){(float)(screenWidth / 2 - MeasureText("1. Nivel Facil", 20) / 2), (float)(screenHeight / 2)}, 20, 0, Fade(BLACK, menuAlpha));
+    DrawTextEx(font, "2. Nivel Dificil", (Vector2){(float)(screenWidth / 2 - MeasureText("2. Nivel Dificil", 20) / 2), (float)(screenHeight / 2 + 30)}, 20, 0, Fade(BLACK, menuAlpha));
+    DrawTextEx(font, "3. Salir", (Vector2){(float)(screenWidth / 2 - MeasureText("3. Salir", 20) / 2), (float)(screenHeight / 2 + 60)}, 20, 0, Fade(BLACK, menuAlpha));
+
+    EndDrawing();
+}
+
+// Actualiza el menú
+void UpdateMenu(void)
+{
+    // Actualiza el desvanecimiento del menú
+    menuAlpha -= 0.01f; // Puedes ajustar este valor para controlar la velocidad del desvanecimiento
+    if (menuAlpha < 2.0f)
+        menuAlpha = 2.0f;
+
+    if (menuFadeOut)
+    {
+        // Realiza acciones específicas según la opción seleccionada
+        if (selectedOption == 1)
+        {
+            InitGame(); // Inicia el juego
+            gameOver = false;
+            gameState = GAME; // Cambia al estado del juego después de seleccionar la opción 1
+        }
+        else if (selectedOption == 2)
+        {
+            // Lógica para la opción 2 del menú
+            // Puedes agregar la funcionalidad específica que desees aquí
+        }
+        else if (selectedOption == 3)
+        {
+            CloseWindow(); // Cierra la ventana después de seleccionar la opción 3
+        }
+
+        menuFadeOut = false; // Restablece la animación de desvanecimiento después de realizar la acción
+    }
+
+    if (IsKeyPressed(KEY_ONE))
+    {
+        // Inicia el juego con desvanecimiento
+        menuFadeOut = true; // Inicia la animación de desvanecimiento cuando se selecciona una opción
+        selectedOption = 1;
+    }
+    else if (IsKeyPressed(KEY_TWO))
+    {
+        // Lógica para la opción 2 del menú
+        // Puedes agregar la funcionalidad específica que desees aquí
+        selectedOption = 2;
+    }
+    else if (IsKeyPressed(KEY_THREE))
+    {
+        // Cierra la ventana con desvanecimiento
+        menuFadeOut = true; // Inicia la animación de desvanecimiento cuando se selecciona una opción
+        selectedOption = 3;
+    }
+}
+
+void ResetGame(void)
+{
+    InitGame();
+    gameOver = false;
+    gameState = MENU;
+    menuFadeOut = false;
+    menuAlpha = 1.0f;
+    selectedOption = 0;
 }
